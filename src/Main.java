17bcf9dfb9b4adc6dcb0e8f6dfc614067c6c995b
sergei <osp11011983@mail.ru>
2024-01-29 12:53:17 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        int sum = 20_000;
+
+        int oneMiles = 20;
+
+        int bonus = sum / 20;
+
+        System.out.println("Начисленные мили: " + bonus);
+    }
 }
